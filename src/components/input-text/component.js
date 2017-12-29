69a26ba2bebb22d14e8887c33a95eebeb929c077
rioarray <rio.array@windowslive.com>
2017-12-29 18:50:38 +0700

    implement global component input select

--- src/components/input-text/component.js
+++ src/components/input-text/component.js
@@ -5,20 +5,16 @@ import { Form, Input } from 'antd';
 export const InputText = ({
   id,
   name,
-  autosize,
   colon,
   defaultValue,
   disabled,
-  enterButton,
   error,
   label,
   labelCol,
-  mode,
   onBlur,
   onChange,
   onFocus,
   onKeyDown,
-  onSearch,
   placeholder,
   prefix,
   required,
@@ -29,7 +25,6 @@ export const InputText = ({
 }) => {
   let showValidationStatus = '';
   let showValidationMessage = '';
-  let showModeInputText = '';
 
   if (error) {
     showValidationStatus = 'error';
@@ -41,57 +36,6 @@ export const InputText = ({
     showValidationMessage = success;
   }
 
-  switch (mode) {
-    case 'text':
-      showModeInputText = (
-        <Input
-          id={id}
-          name={name}
-          placeholder={placeholder}
-          onBlur={onBlur}
-          onChange={onChange}
-          onKeyDown={onKeyDown}
-          onFocus={onFocus}
-          disabled={disabled}
-          defaultValue={defaultValue}
-          value={value}
-          size={size}
-          prefix={prefix}
-        />
-      );
-      break;
-    case 'search':
-      showModeInputText = (
-        <Input.Search
-          id={id}
-          name={name}
-          placeholder={placeholder}
-          onSearch={onSearch}
-          defaultValue={defaultValue}
-          size={size}
-          enterButton={enterButton}
-        />
-      );
-      break;
-    case 'textarea':
-      showModeInputText = (
-        <Input.TextArea
-          id={id}
-          name={name}
-          placeholder={placeholder}
-          onBlur={onBlur}
-          onChange={onChange}
-          onKeyDown={onKeyDown}
-          onFocus={onFocus}
-          disabled={disabled}
-          defaultValue={defaultValue}
-          autosize={autosize}
-        />
-      );
-      break;
-    default:
-      break;
-  }
   return (
     <Form.Item
       colon={colon}
@@ -102,7 +46,20 @@ export const InputText = ({
       validateStatus={showValidationStatus}
       wrapperCol={wrapperCol}
     >
-      {showModeInputText}
+      <Input
+        id={id}
+        name={name}
+        placeholder={placeholder}
+        onBlur={onBlur}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+        onFocus={onFocus}
+        disabled={disabled}
+        defaultValue={defaultValue}
+        value={value}
+        size={size}
+        prefix={prefix}
+      />
     </Form.Item>
   );
 };
@@ -110,26 +67,16 @@ export const InputText = ({
 InputText.propTypes = {
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  autosize: PropTypes.oneOfType([
-    PropTypes.bool,
-    PropTypes.object,
-  ]),
   colon: PropTypes.bool,
   defaultValue: PropTypes.string,
   disabled: PropTypes.bool,
-  enterButton: PropTypes.oneOfType([
-    PropTypes.bool,
-    PropTypes.element,
-  ]),
   error: PropTypes.string,
   label: PropTypes.string,
   labelCol: PropTypes.object,
-  mode: PropTypes.string,
   onBlur: PropTypes.func,
   onChange: PropTypes.func,
   onFocus: PropTypes.func,
   onKeyDown: PropTypes.func,
-  onSearch: PropTypes.func,
   placeholder: PropTypes.string,
   prefix: PropTypes.oneOfType([
     PropTypes.string,
@@ -145,19 +92,16 @@ InputText.propTypes = {
 InputText.defaultProps = {
   id: '',
   name: '',
-  autosize:{ minRows: 2, maxRows: 6 },
   colon: false,
   defaultValue: '',
   disabled: false,
-  enterButton: false,
   error: '',
   label: '',
   labelCol: {},
-  mode: 'text',
+  onBlur: () => {},
   onChange: () => {},
   onFocus: () => {},
   onKeyDown: () => {},
-  onSearch: () => {},
   placeholder: '',
   prefix: '',
   required: false,
@@ -167,6 +111,221 @@ InputText.defaultProps = {
   wrapperCol: {},
 };
 
+export const InputTextArea = ({
+  id,
+  name,
+  autosize,
+  colon,
+  defaultValue,
+  disabled,
+  error,
+  label,
+  labelCol,
+  onBlur,
+  onChange,
+  onFocus,
+  onKeyDown,
+  placeholder,
+  required,
+  success,
+  value,
+  wrapperCol,
+}) => {
+  let showValidationStatus = '';
+  let showValidationMessage = '';
+
+  if (error) {
+    showValidationStatus = 'error';
+    showValidationMessage = error;
+  }
+
+  if (success) {
+    showValidationStatus = 'success';
+    showValidationMessage = success;
+  }
+
+  return (
+    <Form.Item
+      colon={colon}
+      help={showValidationMessage}
+      label={label}
+      labelCol={labelCol}
+      required={required}
+      validateStatus={showValidationStatus}
+      wrapperCol={wrapperCol}
+    >
+      <Input.TextArea
+        id={id}
+        name={name}
+        placeholder={placeholder}
+        onBlur={onBlur}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+        onFocus={onFocus}
+        disabled={disabled}
+        defaultValue={defaultValue}
+        autosize={autosize}
+      />
+    </Form.Item>
+  );
+};
+
+InputTextArea.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  autosize: PropTypes.oneOfType([
+    PropTypes.bool,
+    PropTypes.object,
+  ]),
+  colon: PropTypes.bool,
+  defaultValue: PropTypes.string,
+  disabled: PropTypes.bool,
+  error: PropTypes.string,
+  label: PropTypes.string,
+  labelCol: PropTypes.object,
+  onBlur: PropTypes.func,
+  onChange: PropTypes.func,
+  onFocus: PropTypes.func,
+  onKeyDown: PropTypes.func,
+  placeholder: PropTypes.string,
+  required: PropTypes.bool,
+  success: PropTypes.string,
+  value: PropTypes.string,
+  wrapperCol: PropTypes.object,
+};
+
+InputTextArea.defaultProps = {
+  id: '',
+  name: '',
+  autosize: { minRows: 2, maxRows: 6 },
+  colon: false,
+  defaultValue: '',
+  disabled: false,
+  error: '',
+  label: '',
+  labelCol: {},
+  onBlur: () => {},
+  onChange: () => {},
+  onFocus: () => {},
+  onKeyDown: () => {},
+  placeholder: '',
+  required: false,
+  success: '',
+  value: '',
+  wrapperCol: {},
+};
+
+export const InputSearch = ({
+  id,
+  name,
+  colon,
+  defaultValue,
+  enterButton,
+  error,
+  label,
+  labelCol,
+  onBlur,
+  onChange,
+  onFocus,
+  onKeyDown,
+  onSearch,
+  placeholder,
+  required,
+  size,
+  success,
+  value,
+  wrapperCol,
+}) => {
+  let showValidationStatus = '';
+  let showValidationMessage = '';
+
+  if (error) {
+    showValidationStatus = 'error';
+    showValidationMessage = error;
+  }
+
+  if (success) {
+    showValidationStatus = 'success';
+    showValidationMessage = success;
+  }
+
+  return (
+    <Form.Item
+      colon={colon}
+      help={showValidationMessage}
+      label={label}
+      labelCol={labelCol}
+      required={required}
+      validateStatus={showValidationStatus}
+      wrapperCol={wrapperCol}
+    >
+      <Input.Search
+        id={id}
+        name={name}
+        placeholder={placeholder}
+        onBlur={onBlur}
+        onChange={onChange}
+        onFocus={onFocus}
+        onKeyDown={onKeyDown}
+        onSearch={onSearch}
+        defaultValue={defaultValue}
+        value={value}
+        size={size}
+        enterButton={enterButton}
+      />
+    </Form.Item>
+  );
+};
+
+InputSearch.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  colon: PropTypes.bool,
+  defaultValue: PropTypes.string,
+  enterButton: PropTypes.oneOfType([
+    PropTypes.bool,
+    PropTypes.element,
+  ]),
+  error: PropTypes.string,
+  label: PropTypes.string,
+  labelCol: PropTypes.object,
+  onBlur: PropTypes.func,
+  onChange: PropTypes.func,
+  onFocus: PropTypes.func,
+  onKeyDown: PropTypes.func,
+  onSearch: PropTypes.func,
+  placeholder: PropTypes.string,
+  required: PropTypes.bool,
+  size: PropTypes.string,
+  success: PropTypes.string,
+  value: PropTypes.string,
+  wrapperCol: PropTypes.object,
+};
+
+InputSearch.defaultProps = {
+  id: '',
+  name: '',
+  colon: false,
+  defaultValue: '',
+  enterButton: false,
+  error: '',
+  label: '',
+  labelCol: {},
+  onBlur: () => {},
+  onChange: () => {},
+  onFocus: () => {},
+  onKeyDown: () => {},
+  onSearch: () => {},
+  placeholder: '',
+  required: false,
+  size: 'default',
+  success: '',
+  value: '',
+  wrapperCol: {},
+};
+
 export default {
   InputText,
+  InputTextArea,
+  InputSearch,
 };
